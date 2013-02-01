a775f1f167c7e9e4ab148f33e381136d2c1ecfa4
Ford <github@snosrap.com>
2013-01-31 18:04:49 -0800

    Create csv2html.py

---
+++ csv2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*- 
+
+import sys, csv
+import StringIO
+try:
+  from markdown import markdown
+except ImportError:
+	def markdown(s):
+		return s
+
+stdin_str = sys.stdin.read()
+
+rows = csv.reader(StringIO.StringIO(stdin_str), dialect=csv.Sniffer().sniff(stdin_str))
+
+print "<table>"
+for row in rows:
+	print "<tr>" + "".join(["<td>%s</td>" % markdown(x) for x in row]) + "</tr>"
+print "</table>"
